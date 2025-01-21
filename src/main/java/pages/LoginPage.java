368c552181c8ce9f9109490798e3a666daeeb99b
Samitha Maddekanda <savithrimaddekanda@gmail.com>
2025-01-22 12:30:03 +1300

    Added Allure Reports

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -16,6 +17,7 @@ public class LoginPage {
     private final By usernameField = By.xpath("//input[@placeholder='Username']");
     private final By passwordField = By.xpath("//input[@placeholder='Password']");
     private final By loginButton = By.xpath("//button[normalize-space()='Login']");
+    private final By errorMessage = By.xpath("//p[text()[normalize-space()='Invalid credentials']]");
 
     //Constructor
     public LoginPage(WebDriver driver){
@@ -24,23 +26,37 @@ public class LoginPage {
     }
 
     //Actions
+    @Step("Enter Username: {Username}")
     public void enterUsername(String username){
         wait.until(ExpectedConditions.visibilityOfElementLocated(usernameField));
         driver.findElement(usernameField).sendKeys(username);
     }
 
+    @Step("Enter Password")
     public void enterPassword(String password){
         wait.until(ExpectedConditions.visibilityOfElementLocated(passwordField));
         driver.findElement(passwordField).sendKeys(password);
     }
 
+    @Step("Clock on Login Button")
     public void clickLoginButton(){
         wait.until(ExpectedConditions.elementToBeClickable(loginButton));
         driver.findElement(loginButton).click();
     }
 
+    @Step("Get Page Title")
     public String getPageTitle(){
         return driver.getTitle();
+    }
+
+    @Step("Get Invalid Login Error")
+    public String getErrorMessage(){
+        return driver.findElement(errorMessage).getText();
+    }
+
+    @Step("Check if Dashboard is displayed")
+    public boolean isDashboardDisplayed(){
+        return driver.getCurrentUrl().contains("dashboard");
     }
 
 
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import base.BaseTest;
+import io.qameta.allure.Description;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.LoginPage;
@@ -8,14 +9,28 @@ import pages.LoginPage;
 public class LoginTest extends BaseTest {
 
     @Test
-    public void testValidLogin(){
+    @Description("Test valid login functionality with correct username and password")
+    public void testValidLogin() {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.enterUsername("Admin");
         loginPage.enterPassword("admin123");
         loginPage.clickLoginButton();
 
         //Assertions
-        String expectedTitle = "OrangeHRM";
-        Assert.assertEquals(loginPage.getPageTitle(),expectedTitle,"Login Failed....");
+        Assert.assertTrue(loginPage.isDashboardDisplayed(),"Redirected to Dashboard upon successful login");
+
     }
+
+    @Test
+    @Description("Test invalid login functionality with incorrect username and password")
+    public void testInvalidLogin(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.enterUsername("Sam");
+        loginPage.enterPassword("123");
+        loginPage.clickLoginButton();
+
+        String invalidLogin = loginPage.getErrorMessage();
+        Assert.assertEquals(invalidLogin,"Invalid credentials", "Unable to login: Invalid credentials");
+    }
+
 }
